de8e4c55168b957f3a5b4d4d6b6e5a762106ee9b
Olubanwo Ayomide <50876658+ayobanwo@users.noreply.github.com>
2024-06-05 11:03:31 +0100

    pin

--- src/components/Dashboard/Account/Security/CreatePIN.tsx
+++ src/components/Dashboard/Account/Security/CreatePIN.tsx
@@ -1,17 +1,62 @@
 import { ArrowLeft } from 'iconsax-react'
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
+import PinInput from 'react-pin-input';
 
 const CreatePIN = () => {
     const navigate = useNavigate();
+    const [onCreate, setOnCreate] = useState(false)
+    const handleOnCreate = (value: any) => {
+        setOnCreate(true)
+    }
     return (
         <div className="px-6 pt-8 pb-14 flex flex-col text-white bg-purpleX min-h-[100vh] ">
             <div onClick={() => navigate('/security')}>
                 <ArrowLeft size="24" color='#FFFFFF' />
             </div>
             <div className="my-8">
-                <h1 className="text-white font-semibold text-2xl">Create transaction PIN</h1>
+                <h1 className="text-white font-semibold text-2xl">{onCreate? 'Confirm' : 'Create'} transaction PIN</h1>
                 <span className="text-orange text-xs">Create a 4 digit PIN that is unique to you . Your PIN will be used to confirm your transactions</span>
+            </div>
+            <div className="flex justify-center items-start gap-8">
+                {
+                    !onCreate?
+                    <PinInput 
+                        length={4} 
+                        initialValue=""
+                        secret
+                        secretDelay={100} 
+                        onChange={(value, index) => {}} 
+                        type="numeric" 
+                        inputMode="number"
+                        style={{padding: '10px'}}  
+                        inputStyle={{borderColor: '#2E5BCC', borderRadius: '5px', width: "100%"}}
+                        inputFocusStyle={{background: '#2E5BCC'}}
+                        onComplete={(value, index) => {handleOnCreate(value)}}
+                        autoSelect={true}
+                        regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+                    />  
+                    
+                    :
+
+                    <PinInput 
+                        length={4} 
+                        initialValue=" "
+                        secret
+                        secretDelay={100} 
+                        // onChange={(value, index) => {}} 
+                        type="numeric" 
+                        inputMode="number"
+                        style={{padding: '10px'}}  
+                        inputStyle={{borderColor: '#ffffff', borderRadius: '5px', width: "100%"}}
+                        inputFocusStyle={{background: '#ffffff'}}
+                        // onComplete={(value, index) => {}}
+                        autoSelect={true}
+                        regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+                    />   
+                }
+
+
             </div>
         </div>
     )
